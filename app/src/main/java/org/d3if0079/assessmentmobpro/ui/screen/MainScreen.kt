d60ae968e91f69d55aee0fa1be3b045199d1cffc
Feby Irmayana <febyirmayani@gmail.com>
2024-04-02 18:17:01 +0700

    Membuat perhitungan zakat dan Memastikan input sesuai harapan

--- app/src/main/java/org/d3if0079/assessmentmobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0079/assessmentmobpro/ui/screen/MainScreen.kt
@@ -14,8 +14,12 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -25,8 +29,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -63,13 +70,16 @@ fun MainScreen() {
 @Composable
 fun ScreenContent(modifier: Modifier) {
     var jumlah by remember { mutableStateOf("") }
-    var nominal by remember { mutableStateOf("")}
-    
+    var jumlahError by remember { mutableStateOf(false) }
+
     val radioOptions = listOf(
         stringResource(id = R.string.uang),
         stringResource(id = R.string.beras)
     )
     var pilihan by remember { mutableStateOf(radioOptions[0]) }
+
+    var kategori by rememberSaveable { mutableIntStateOf(0) }
+    var totalHarga by rememberSaveable { mutableFloatStateOf(0f) }
 
     Column (
         modifier = modifier
@@ -88,17 +98,8 @@ fun ScreenContent(modifier: Modifier) {
             value = jumlah,
             onValueChange = { jumlah = it },
             label = { Text(text = stringResource(R.string.jumlah_orang)) },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-        OutlinedTextField(
-            value = nominal,
-            onValueChange = { nominal = it },
-            label = { Text(text = stringResource(R.string.nominal_pembayaran)) },
+            isError = jumlahError,
+            supportingText = { ErrorHint(jumlahError) },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
@@ -112,7 +113,7 @@ fun ScreenContent(modifier: Modifier) {
                 .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
         ) {
             radioOptions.forEach { text ->
-                GenderOption(
+                ZakatOption(
                     label = text,
                     isSelected = pilihan == text,
                     modifier = Modifier
@@ -127,17 +128,46 @@ fun ScreenContent(modifier: Modifier) {
             }
         }
         Button(
-            onClick = {},
+            onClick = {
+                jumlahError = (jumlah == "" || jumlah == "0")
+                if (jumlahError ) return@Button
+
+                totalHarga = hitungZakat(jumlah.toFloat(), pilihan==radioOptions[0])
+            },
             modifier = Modifier.padding(top = 8.dp),
             contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
         ) {
             Text(text = stringResource(R.string.hitung))
         }
+
+        if (totalHarga !=0f) {
+            Divider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 1.dp
+            )
+            Text(text = stringResource(id = R.string.rumus, totalHarga))
+        }
     }
 }
 
 @Composable
-fun GenderOption(label: String, isSelected: Boolean, modifier: Modifier) {
+fun IconPicker(isError: Boolean, unit: String) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    } else {
+        Text(text = unit)
+    }
+}
+
+@Composable
+fun  ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = stringResource(R.string.input_invalid))
+    }
+}
+
+@Composable
+fun ZakatOption(label: String, isSelected: Boolean, modifier: Modifier) {
     Row (
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
@@ -148,6 +178,15 @@ fun GenderOption(label: String, isSelected: Boolean, modifier: Modifier) {
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.padding(start = 8.dp)
         )
+    }
+}
+
+private fun hitungZakat(jumlah: Float, isRice: Boolean): Float {
+    return if(isRice) {
+        return jumlah * 40000
+    }
+    else {
+        return jumlah / 1000 * 2700
     }
 }
 

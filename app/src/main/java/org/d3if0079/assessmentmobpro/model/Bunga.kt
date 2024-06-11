e8f63949e10b3ab13643b38b7874e84689a24e27
Feby Irmayana <febyirmayani@gmail.com>
2024-06-11 22:34:58 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/org/d3if0079/assessmentmobpro/model/Bunga.kt
@@ -0,0 +1,7 @@
+package org.d3if0079.assessmentmobpro.model
+
+data class Bunga(
+    val nama: String,
+    val namaLatin: String,
+    val imageId: String
+)
--- app/src/main/java/org/d3if0079/assessmentmobpro/network/BungaApiService.kt
+++ app/src/main/java/org/d3if0079/assessmentmobpro/network/BungaApiService.kt
@@ -1,18 +1,25 @@
 package org.d3if0079.assessmentmobpro.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.d3if0079.assessmentmobpro.model.Bunga
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://raw.githubusercontent.com/Feby2004/Bunga-Json/main/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 interface BungaApiService {
     @GET("static-api.Json")
-    suspend fun getBunga(): String
+    suspend fun getBunga(): List<Bunga>
 }
 
 object BungaApi {
